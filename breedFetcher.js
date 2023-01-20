fa558aa381ac8fa3717cc957bd1ddfc26aec7f43
woodenonesie <olga.pi4ugina@yandex.ru>
2023-01-19 21:36:17 -0800

    added one more test, code has been refactored

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,16 @@
 const request = require('request');
 
-const args = process.argv.slice(2);
-let breed = args[0];
-const id = breed.slice(0, 4);
+const fetchBreedDescription = function (breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    console.log('statusCode:', response && response.statusCode);
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback(error, null);
+    } else {
+      let description = data[0]['description'];
+      callback(error, description);
+    }
+  });
+};
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + id, (error, response, body) => {
-  console.log('statusCode:', response && response.statusCode);
-  if (error !== null) {
-    console.log("Request failed");
-    process.exit();
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log("Breed not found");
-  } else {
-    let description = data[0]['description'];
-    console.log(description);
-  }
-});
+module.exports = { fetchBreedDescription };
